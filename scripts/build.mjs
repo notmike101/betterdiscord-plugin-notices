417c717121d4bb98da9a3eb112ba8c067798e4ee
Mike Orozco <me@mikeorozco.dev>
2022-05-28 18:18:41 -0700

    Remove unnecessry steps from build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,18 +1,11 @@
 import { build } from 'esbuild';
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
-import fs from 'fs';
-
-const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 const config = {
   entryPoints: ['./src/index.ts'],
   bundle: true,
-  external: ['betterdiscord'],
   sourcemap: true,
   plugins: [dtsPlugin()],
-  define: {
-    'process.env.VERSION': JSON.stringify(packageJson.version),
-  },
 };
 
 async function main() {
